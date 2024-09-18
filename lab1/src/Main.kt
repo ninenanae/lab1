79bd91228641fd32fb44de53456869d5877f253b
ninenanae <1010.docx@gmail.com>
2024-09-18 09:18:43 +0900

    Add files via upload

---
+++ lab1/src/Main.kt
@@ -0,0 +1,67 @@
+fun main()
+{
+    println("ЗАДАНИЕ 1")
+
+    println("\nЗАДАНИЕ 2")
+    val число0 = 3.14159265358979323846
+    println(String.format("%.2f", число0))
+
+    println("\n4ЗАДАНИЕ 3")
+    val e = Math.E
+    println(String.format("%.1f", e))
+
+    println("\nЗАДАНИЕ 4")
+    print("Введите число: ")
+    val число1 = readLine()!!.toInt()
+    println("Вы ввели число: $число1")
+
+    println("\nЗАДАНИЕ 5")
+    print("Введите число: ")
+    val число2 = readLine()!!.toInt()
+    println("$число2 — вот какое число Вы ввели")
+
+    println("\nЗАДАНИЕ 6")
+    println("1 13 49")
+
+    println("\nЗАДАНИЕ 7")
+    println("7  15  100")
+
+    println("\nЗАДАНИЕ 8")
+    val число3a = readLine()!!.toInt()
+    val число3b = readLine()!!.toInt()
+    val число3c = readLine()!!.toInt()
+    println("$число3a  $число3b  $число3c")
+
+    println("\nЗАДАНИЕ 9")
+    val число4a = readLine()!!.toInt()
+    val число4b = readLine()!!.toInt()
+    val число4c = readLine()!!.toInt()
+    val число4d = readLine()!!.toInt()
+    println("$число4a  $число4b  $число4c  $число4d")
+
+    println("\nЗАДАНИЕ 10")
+    println("50")
+    println("10")
+
+    println("\nЗАДАНИЕ 11")
+    println("5")
+    println("10")
+    println("21")
+
+    println("\nЗАДАНИЕ 12")
+    val число5a = readLine()!!.toInt()
+    val число5b = readLine()!!.toInt()
+    val число5c = readLine()!!.toInt()
+    val число5d = readLine()!!.toInt()
+    println("$число5a")
+    println("$число5b")
+    println("$число5c")
+    println("$число5d")
+
+
+
+
+
+}
+
+
